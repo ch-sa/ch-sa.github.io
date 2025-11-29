7f704be2e3454b9480490a29d03d971774cd7f8d
ch-sa <christoph.sager@gmail.com>
2025-11-29 11:46:54 +0100

    More concise cards

--- tools/feiertags-diff/script.js
+++ tools/feiertags-diff/script.js
@@ -176,11 +176,13 @@ function compareHolidays(data) {
     function addHolidayCell(stateName, cellTd) {
       if (isHolidayInState(stateName)) {
         cellTd.textContent = holiday.nameDe;
+        cellTd.setAttribute("data-mobile-content", "✓");
         if (stateName !== currentState && !isHolidayInState(currentState)) {
           cellTd.classList.add("added");
         }
       } else {
         cellTd.textContent = "Kein Feiertag";
+        cellTd.setAttribute("data-mobile-content", "✗");
         cellTd.classList.add("removed");
       }
       tr.appendChild(cellTd);
@@ -188,8 +190,10 @@ function compareHolidays(data) {
 
     // Header column
     const dateTd = document.createElement("td");
-    dateTd.setAttribute("data-label", "Date");
-    dateTd.textContent = `${holiday.date} (${getWeekdaySuffix(holiday.date)})`;
+    const dateString = `${holiday.date} (${getWeekdaySuffix(holiday.date)})`;
+    dateTd.setAttribute("data-label", dateString);
+    dateTd.setAttribute("data-holiday-name", holiday.nameDe);
+    dateTd.textContent = dateString;
     tr.appendChild(dateTd);
 
     // Base column
